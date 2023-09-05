b52b63fc7edbace0e89c966bec3864b5f3abf84c
techcaptain83 <tech.captain83@gmail.com>
2023-09-04 22:17:48 -0700

    env file update

--- src/main.ts
+++ src/main.ts
@@ -11,37 +11,15 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  // app.enableCors({
+    // origin: configService.get('CLIENT_URL'),
+  // });
   app.enableCors({
-    origin: configService.get('CLIENT_URL'),
-  });
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+});
   const port = configService.get('PORT') || 3000;
   await app.listen(port);
 }
 bootstrap();
-
-
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ConfigService } from '@nestjs/config';
-// import { ValidationPipe } from '@nestjs/common';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   const configService = app.get(ConfigService);
-
-//   // Whitelist the IP address used by Jira
-//   const jiraIpAddress = 'https://focusbear.atlassian.net'; // Replace with the actual IP address used by Jira
-//   app.enableCors({
-//     origin: '*',
-//   });
-
-//   app.useGlobalPipes(
-//     new ValidationPipe({
-//       whitelist: true,
-//     }),
-//   );
-
-//   const port = configService.get('PORT') || 3000;
-//   await app.listen(port);
-// }
-// bootstrap();
