8ad0a3bb406f6fd65b79e80305753c508f7458a3
techcaptain83 <tech.captain83@gmail.com>
2023-09-14 08:10:47 -0700

    cors update

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,20 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //   }),
+  // );
+    // app.use((req, res, next) => {
+    //     res.header('Access-Control-Allow-Origin', '*');
+    //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    //     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    //     next();
+    // });
   app.enableCors({
-    origin: configService.get('CLIENT_URL'),
+      allowedHeaders:"*",
+      origin: "*"
   });
   const port = configService.get('PORT') || 3000;
   await app.listen(port);
