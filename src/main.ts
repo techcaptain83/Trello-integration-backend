0c6b23d72479794376d8df70c5bb623ebf81c7ba
techcaptain83 <tech.captain83@gmail.com>
2023-09-14 08:39:08 -0700

    clear code

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //   }),
-  // );
-    // app.use((req, res, next) => {
-    //     res.header('Access-Control-Allow-Origin', '*');
-    //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    //     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    //     next();
-    // });
+
   app.enableCors({
       allowedHeaders:"*",
       origin: "*"
